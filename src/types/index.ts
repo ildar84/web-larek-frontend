1c9307262a9a05cd8eebbc46a87de6e139e0038a
Ildar <isuncheleev@yandex.ru>
2024-10-06 20:22:55 +0300

    описаны типы данных

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+
+
+export interface IProduct {
+    id: string;
+    image: string;
+    title: string;
+    description: string;
+    category: string;
+    price: number;
+}
+
+export interface IProductData {
+    products: IProduct[];
+    preview: string | null;
+}
+
+export interface IBasket {
+    items: IProduct[];
+    total: number;
+}
+
+export interface IOrder {
+    address: string;
+    payment: string;
+    email: string;
+    phone: string;
+}
+
+export type TOrderInfo = Pick<IOrder, 'address' | 'payment'>; 
+
+export type TClientinfo = Pick<IOrder, 'email'|'phone'>; 
